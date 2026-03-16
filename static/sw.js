cd6156110a08c6fee79b31fab392a569c3fd5a82
Ananevych Vladyslav <ananevychvlad@gmail.com>
2026-03-16 21:41:37 +0200

    Updating offline mode

--- static/sw.js
+++ static/sw.js
@@ -1,4 +1,5 @@
-const CACHE_NAME = 'liquid-glass-pwa-v1';
+const CACHE_NAME = 'mykep-pwa-v1';
+const DYNAMIC_CACHE = 'mykep-dynamic-v1';
 const ASSETS_TO_CACHE = [
     '/',
     '/static/index.html',
@@ -15,8 +16,21 @@ self.addEventListener('install', event => {
 });
 
 self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request)
-        .then(response => response || fetch(event.request))
-    );
+    if (event.request.url.includes('/api/schedule')) {
+        event.respondWith(
+            fetch(event.request)
+                .then(response => {
+                    return caches.open(DYNAMIC_CACHE).then(cache => {
+                        cache.put(event.request.url, response.clone());
+                        return response;
+                    });
+                })
+                .catch(() => caches.match(event.request))
+        );
+    } else {
+        event.respondWith(
+            caches.match(event.request)
+                .then(response => response || fetch(event.request))
+        );
+    }
 });
